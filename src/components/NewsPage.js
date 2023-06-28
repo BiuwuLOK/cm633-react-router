d999f9bc27b2e7627034ffeb8400fdfefeb2875c
たびチュの狐狸先生 <47691322+senkoraku552@users.noreply.github.com>
2023-06-28 21:48:26 +0800

    up

--- src/components/NewsPage.js
+++ src/components/NewsPage.js
@@ -3,14 +3,14 @@ import { useLoaderData } from "react-router-dom";
 
 const NewsPage = () => {
   const { data } = useLoaderData();
-  console.log(data);
+  // console.log(data);
 
   return (
     <>
       <h1 className="text-6xl mb-2">This is the NewsPage</h1>
       <ul className="list-disc grid gap-4 text-lg">
         {data.map((data) => (
-          <li className="mx-6 underline hover:text-blue-400">
+          <li className="mx-6 underline hover:text-blue-400" key={data.id}>
             <NavLink
               className={({ isPending }) =>
                 `${isPending ? "bg-yellow-200" : ""}`
